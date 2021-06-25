d3e42a06810ccf46ea830dcd396ff9e852287ee1
rajan <rajanwalia4@gmail.com>
2021-06-25 17:57:03 +0530

    changes in Arrays

--- 2 arrays/script.js
+++ 2 arrays/script.js
@@ -1,23 +1,28 @@
 
 //Create an array
 // a =[1,2,3];
-// console.log(a);
-// console.log(a.length);
-// console.log(a[0],a[5]);
+// console.log(a);				// [1,2,3]
+// console.log(a.length);		// 3
+// console.log(a[0],a[5]);		// 1 undefined
 // a[5] = 34;
 
-// console.log(a);
-// console.log(a[4]=33);
-// console.log(a)
+// console.log(a);				// [1, 2, 3, empty × 2, 34]
+// console.log(a[4]=33);		// 33
+// console.log(a)				// [1, 2, 3, empty, 33, 34]
 
 // create an array using new keyword
 
 //create  pushes the element in the array
 // a = new Array(1,2,"raja")
-// console.log(a);
+// console.log(a);					// [1, 2, "raja"]
 // //create 2d array
 // a= new Array(a,a);
-// console.log(a)
+// console.log(a)					// [[1, 2, "raja"], [1, 2, "raja"]]
+// console.log(a.length);			// 2
+// console.log(a[1][2])			// 3
+// //console.log(a[2][2])			// TypeError: Cannot read property '2' of undefined
+// console.log(a[1].length);		// 3
+
 
 // here 2 is the size of array
 // a = new Array(2);
@@ -42,14 +47,14 @@
 // console.log(a);
 
 
-// a = [12,23,43,56]
+//a = [12,23,43,56]
 
-// a.splice(1,1); // delete 1 element form 1st index
-// console.log(a);
-// a.splice(1,0,"rajan","walia") // insert first element from 1st index
+//a.splice(1,1); // delete 1 element form 1st index
+//console.log(a);	// [12,43,56]
+// a.splice(1,0,"rajan","walia") // insert first element from 1st index and 0 shows not item delete
 // console.log(a);
 
-// console.log(a.splice(2,23,122));
+// console.log(a.splice(2,23,122)); // delete 23 elements from 2nd index and then insert 122 at 2nd index
 // console.log(a);
 
 
@@ -58,7 +63,8 @@
 // for(let i=0;i<a.length;i++)
 // 	console.log(a[i])
 	
-	
+
+ a = [12,23,22,33]
 // console.log("\n")
 // function print(e){
 // 	console.log(e);
@@ -73,3 +79,5 @@
 //  console.log("\n")
 //  a.forEach((e)=>{ console.log(e)
 //  })
+
+// a.forEach( e => console.log(e));

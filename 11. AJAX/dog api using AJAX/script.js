6faf5a881f04db9a7177255c20a9a9b77d4cf173
rajan <rajanwalia4@gmail.com>
2021-07-18 18:37:07 +0530

    added promise in JS

--- 11. AJAX/dog api using AJAX/script.js
+++ 11. AJAX/dog api using AJAX/script.js
@@ -2,24 +2,29 @@ var fetchBtn = document.getElementById("fetchBtn");
 var dogImg = document.getElementById("dogImg");
 
 // XHR AJAX using JS
-// fetchBtn.addEventListener("click",function(){
-// 	var xhr = new XMLHttpRequest();
-// 	// true is used to make Asynchronous calls
-// 	xhr.open("GET","https://dog.ceo/api/breeds/image/random",true);
-// 	xhr.send();
-// 	xhr.onload = function(){
-// 		if(this.status == 200){
-// 			// here this is a xhr object
-// 			var responseJSON = JSON.parse(this.response);
-// 			// console.log(responseJSON.message); 
-// 			var imgURL = responseJSON.message;
-// 			dogImg.setAttribute("src",imgURL);
-// 		}else{
-// 			console.log("Response not found")
-// 		}
-// 	}
-// 	console.log("hello");
-// });
+fetchBtn.addEventListener("click",function(){
+	var xhr = new XMLHttpRequest();
+
+	xhr.onload = function(){
+		if(this.status == 200){
+			// here this is a xhr object
+			var responseJSON = JSON.parse(this.response);
+			// console.log(responseJSON.message); 
+			var imgURL = responseJSON.message;
+			dogImg.setAttribute("src",imgURL);
+		}else{
+			console.log("Response not found")
+		}
+	};
+	
+	xhr.onerror = function(){
+		console.log("Response Fail");
+	};
+	// true is used to make Asynchronous calls
+	xhr.open("GET","https://dog.ceo/api/breeds/image/random",true);
+	xhr.send();
+	console.log("hello");
+});
 
 
 
@@ -28,20 +33,24 @@ var dogImg = document.getElementById("dogImg");
 // 	e.preventDefault();
 // 	$.ajax({
 // 		method: "GET",
-// 		url: "https://dog.ceo/api/breeds/image/random",
+// 		url: "https://dog.ceo/api/breeds/imagee/random",
 // 		success: function (data) {
 // 			var imgURL = data.message;
 // 			$("#dogImg").attr("src",imgURL);
 // 		}
-// 	});
+// 	}).fail(function (){
+// 		console.log("Response fail");
+// 	})
 // });
 
 // Direct AJAX get request using JQuery
-$("#fetchBtn").click(function (e) {
-	$.get("https://dog.ceo/api/breeds/image/random",
-		function (data) {
-			var imgURL = data.message;
-			$("#dogImg").attr("src", imgURL);
-		}
-	);
-});
+// $("#fetchBtn").click(function (e) {
+// 	$.get("https://dog.ceo/api/breeds/image/random",
+// 		function (data) {
+// 			var imgURL = data.message;
+// 			$("#dogImg").attr("src", imgURL);
+// 		}
+// 	).fail(function (){
+// 		console.log("Response fail");
+// 	})
+// });
---
+++ 12. promise/script.js
@@ -0,0 +1,94 @@
+// var userLoggedIn = true;
+
+// var promise = new Promise( (resolve,reject)=>{
+// 	// wait for 2 sec
+// 	setTimeout( ()=>{
+// 		// Promise is resolved
+// 		if(userLoggedIn)
+// 			resolve();
+// 		else
+// 			reject();
+// 	},2000)
+	
+// })
+
+// setTimeout(()=>{
+// 	userLoggedIn = false;
+// },1000);
+
+// promise.then(()=>{ // then is for resolve
+// 	console.log("User Logged IN");
+// }).catch(()=>{ // catch is for reject
+// 	console.log("user not loggedIn"); 
+// })
+
+// In general we wrap the Promise in function and that function returns the promise
+var userLoggedIn = true;
+function checkUserLoggedIn(){
+	var promise = new Promise( (resolve,reject)=>{
+		// wait for 2 sec
+		setTimeout( ()=>{
+			// Promise is resolved
+			if(userLoggedIn)
+				resolve("User   Logged In");
+			else
+				reject("User Not logged In");
+		},2000);
+		
+	});
+	return promise; // returning promise	
+}
+
+setTimeout(()=>{
+	userLoggedIn = false;
+},1000);
+// console.log(checkUserLoggedIn()); print promise object
+
+checkUserLoggedIn().then((successMsg)=>{ // then is for resolve
+	console.log(successMsg);
+}).catch((failureMsg)=>{ // catch is for reject
+	console.log(failureMsg); 
+})
+
+
+// where promise is used
+// Promise solves a problem of asynchoronous chaining request 
+/* 
+ following is the pseudo code of aysynchrounous chaining
+	$.ajax({
+		success: function (response) {
+			$.ajax({
+		success: function (response) {
+			$.ajax({
+					success: function (response) {
+						
+					}
+				});
+			}
+		});
+	}
+});
+*/
+
+// we can achieve the same above thing using promises in JS
+//  promise.then().then().then().then().catch()
+
+/* promise in real life let say we have a social media website
+	how user logged in and what happens
+	1. User LogIn
+	2. fetch user feeds
+	3. fetch User friends
+	4. fetch User Messages etc.
+	
+	we can achieve this thing using promise
+	
+	checkUserLoggedIn().then(fetchUserFeeds).then(fetchUserFriends).then(fetchUserMessages).catch();
+	here checkUserLoggedIn() returns a promise 
+	then checkUserLoggedIn().then(fetchUserFeeds)  this also returns a  promise
+	then checkUserLoggedIn().then(fetchUserFeeds).then(fetchUserFriends) this also returns a promise
+	checkUserLoggedIn().then(fetchUserFeeds).then(fetchUserFriends).then(fetchUserMessages) returns a promise
+	
+	if any of the promise is rejected then no function is called in then 
+	catch is called for the reject
+	
+*/
